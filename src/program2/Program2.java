247892f322ae97dee140dad2c7e309df5061998a
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 20:40:45 +0530

    calculate rupees and paise

--- src/program2/Program2.java
+++ src/program2/Program2.java
@@ -6,6 +6,11 @@ public class Program2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double priceInDecimal = scanner.nextDouble();
+        priceInDecimal *= 100;
+        int paise = (int) priceInDecimal % 100;
+        priceInDecimal /= 100;
+        int rupees = (int) priceInDecimal;
+
 
 
 
