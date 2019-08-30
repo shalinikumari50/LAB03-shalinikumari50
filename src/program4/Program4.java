8cad4be10a2785c486638ea5db92c81f481d906e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 21:52:03 +0530

    add for loop to print the pattern

--- src/program4/Program4.java
+++ src/program4/Program4.java
@@ -6,6 +6,13 @@ public class Program4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+        }
 
         scanner.close();
     }
