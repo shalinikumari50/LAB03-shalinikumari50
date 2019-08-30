e03afd8f50c123d0531fd35c55feb68f816a9f27
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 20:27:42 +0530

    input variable n(int) and initialize sum(double)

--- src/program1/Program1.java
+++ src/program1/Program1.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Program1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double sum = 0;
+
 
         scanner.close();
     }
