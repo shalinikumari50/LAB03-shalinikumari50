7fc7cd13e6dadae93a31dbeaccc949f7e2dcc6f7
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 20:32:03 +0530

    add object:scanner of class:Scanner

--- src/program2/Program2.java
+++ src/program2/Program2.java
@@ -1,5 +1,12 @@
 package program2;
 
+import java.util.Scanner;
+
 public class Program2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 
 }
