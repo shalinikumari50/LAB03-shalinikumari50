1b9a9fff398dcdfcd1c67407f0d03ef133d8b603
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-31 00:05:03 +0530

    input variables date(int),month(int) and year(int)

--- src/program5/Program5.java
+++ src/program5/Program5.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Program5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("date: ");
+        int date = scanner.nextInt();
+        System.out.print("month: ");
+        int month = scanner.nextInt();
+        System.out.print("year: ");
+        int year = scanner.nextInt();
+
         scanner.close();
     }
 }
