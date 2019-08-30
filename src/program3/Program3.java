44e876542100c548ae37e50bd4724e382bc077c5
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 21:40:24 +0530

    calculate temperatureInCelsius(double)

--- src/program3/Program3.java
+++ src/program3/Program3.java
@@ -6,6 +6,8 @@ public class Program3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double temperatureInFahrenheit = scanner.nextDouble();
+        double temperatureInCelsius = (temperatureInFahrenheit - 32) / 1.8;
+
 
 
         scanner.close();
