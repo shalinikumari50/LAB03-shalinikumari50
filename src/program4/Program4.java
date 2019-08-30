fd107bebdfe5bd77380a385b7803f626afff96d8
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 21:51:39 +0530

    input variable n(int)

--- src/program4/Program4.java
+++ src/program4/Program4.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Program4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
         scanner.close();
     }
 }
