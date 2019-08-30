538d8b4e4cf981267377ab94ceaeaee84bf25aea
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 20:39:37 +0530

    input variable priceInDecimal(double)

--- src/program2/Program2.java
+++ src/program2/Program2.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Program2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double priceInDecimal = scanner.nextDouble();
+
+
+
 
         scanner.close();
     }
