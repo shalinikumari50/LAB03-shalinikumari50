4ed358c2642dfdf4d5415ac0cc44dbcd978d345c
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-30 21:25:04 +0530

    add object:scanner for class:Scanner

--- src/program3/Program3.java
+++ src/program3/Program3.java
@@ -1,4 +1,11 @@
 package program3;
 
+import java.util.Scanner;
+
 public class Program3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 }
